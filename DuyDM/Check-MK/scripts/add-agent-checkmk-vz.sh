b538135d6d780ee3d8fa082995d2a4e796031c11
domanhduy <39370225+domanhduy@users.noreply.github.com>
2019-06-18 11:49:58 +0700

    update

--- DuyDM/Check-MK/scripts/add-agent-checkmk-vz.sh
+++ DuyDM/Check-MK/scripts/add-agent-checkmk-vz.sh
@@ -2,7 +2,7 @@
 #duydm
 #Install Agent Check_Mk on Centos7 cum VZ
 
-echocolor "Nhap ip Check_Mk server 103.101.161.204"
+echo "Nhap ip Check_Mk server 103.101.161.204"
 sleep 3
 read ipserver
 
@@ -16,17 +16,17 @@ cp /etc/xinetd.d/check_mk /etc/xinetd.d/check_mk.bk
 sed -i 's/#only_from      = 127.0.0.1 10.0.20.1 10.0.20.2/only_from      = '$ipserver'/g'  /etc/xinetd.d/check_mk
 systemctl restart xinetd
 systemctl status xinetd
-
 echo "Cai dat agent check_mk Ok"
 
-echocolor "Cai dat Check_Mk Inventory"
+##########################################
+echo "Cai dat Check_Mk Inventory"
 sleep 3
 
 wget -O /usr/lib/check_mk_agent/local/mk_inventory  http://mon.cloud365.vn/mon/check_mk/agents/plugins/mk_inventory.linux
 chmod +x /usr/lib/check_mk_agent/local/mk_inventory  
 
-
-echocolor "Cai dat smart disk"
+##########################################
+echo "Cai dat smart disk"
 sleep 3
 
 yum install smartmontools -y
@@ -34,10 +34,9 @@ cd /usr/lib/check_mk_agent/plugins
 wget https://raw.githubusercontent.com/domanhduy/ghichep/master/DuyDM/Check-MK/scripts/smart
 chmod +x smart
 
-
+##########################################
 
 echo "Test cd /usr/lib/check_mk_agent/plugins ./smart"
-
 echo "Test cd /usr/lib/check_mk_agent/local ./mk_inventory"
 
 
